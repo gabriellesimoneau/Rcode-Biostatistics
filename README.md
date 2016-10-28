459b73a4a41fc594e7c7165e50a7adc903b39dcb
Gabrielle Simoneau <gabrielle.simoneau@gmail.com>
2016-10-28 17:58:00 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Rcode-Biostatistics
-## Script to reproduce the results in TITLE 
+# Scripts and material to reproduce the results in *Non-regular Inference for Dynamic Weighted Ordinary Least Squares: Understanding the Impact of Solid Food Intake in Infancy on Childhood Weight*
+
+A dynamic treatment regime (DTR) is a set of decision rules to be applied across multiple stages of treatments. The decisions are tailored to individuals, by inputing an individual?s observed characteristics and outputting a treatment decision at each stage for that individual. Dynamic weighted ordinary least squares (dWOLS) is a theoretically robust and easily implementable method for estimating an optimal DTR. As many related DTR methods, the dWOLS treatment effects estimators can be non-regular when true treatment effects are zero or very small, which results in invalid Wald-type or standard bootstrap confidence intervals. We propose the use the *m*-out-of-*n* bootstrap with dWOLS for valid inferences of optimal DTR. We demonstrate our methodology in a real application, analyzing the effect of diet in infancy on measures of weight and body size in later childhood.
+
+## Simulations
+The objective of our simulation is to evaluate the performance of the *m*-out-of-*n* bootstrap with dWOLS as method of analysis. Here, we provide all R scripts necessary to reproduce the results found in the paper, specifically Table 2 and Figure 1. 
+
+Scenario 	| Seed
+-----------  	| ------------
+1		| 18513
+2		| 2101
+3		| 1070
+4		| 1990
+5		| 6980
+6		| 29340
+7		| 777
+8		| 10010
+9		| 1
+
+
+
+## Application
+In the paper, we demonstrate the proposed methodology using the data from the PROmotion of Breastfeeding Intervention Trial (PROBIT). As the PROBIT dataset can not be shared, we create an example dataset and sample outputs to show how to reproduce the analyses presented in the paper.
+
---
+++ Simulations/R code/mn0.1_allscenarios.R
@@ -0,0 +1,125 @@
+###################################################################################
+install.packages("DTRreg")
+library(DTRreg)
+# source("dtrreg_fun.R")
+
+expit <- function(x) exp(x)/(1+exp(x))
+set.seed(210890)
+
+# gamma parameters following Chakraborty et al (2013) to control for irregularity in the generated data
+g <- matrix(NA, nrow = 9, ncol = 7)
+g[1,] <- c(0,0,0,0,0,0,0)
+g[2,] <- c(0,0,0,0,0.01,0,0)
+g[3,] <- c(0,0,-0.5,0,0.5,0,-0.5) # psi_{10} = -1
+g[4,] <- c(0,0,-0.5,0,0.99,0,-0.98) # psi_{10} = -1.48
+g[5,] <- c(0,0,-0.5,0,1,0.5,-0.5) # psi_{10} = -1
+g[6,] <- c(0,0,-0.5,0,0.25,0.5,0.5) # psi_{10} = -0.081
+g[7,] <- c(0,0,-0.25,0,0.75,0.5,0.5) # psi_{10} = 0.299834
+g[8,] <- c(0,0,0,0,1,0,-1) # psi_{10} = -1
+g[9,] <- c(0,0,0,0,0.25,0,-0.24) # psi_{10} = -0.24
+
+# delta parameters following Chakraborty et al (2013) to control for irregularity in the generated data
+d <- matrix(NA, nrow = 9, ncol = 2)
+d[1,] <- c(0.5,0.5)
+d[2,] <- c(0.5,0.5)
+d[3,] <- c(0.5,0.5)
+d[4,] <- c(0.5,0.5)
+d[5,] <- c(1,0)
+d[6,] <- c(0.1,0.1)
+d[7,] <- c(0.1,0.1)
+d[8,] <- c(0,0)
+d[9,] <- c(0,0)
+
+# extract is a function to extract the main effect of treatment psi_{10} i.e. (effect of treatment first stage) 
+# maybe psi_{11} as well??
+extract <-function(out)
+{
+  psi10 <-out["psi"][[1]][[1]][1] 
+  # psi11 <-out["psi"][[1]][[1]][2]
+  return(psi10)
+}
+
+######################### m-out-of-n bootstrap : fixed alpha #############################
+
+# fixed alpha 
+alpha <- 0.1
+# scenario id
+sc <- seq(1,9)
+# number of simulated dataset
+Nsimul <- 1000 
+# number of boostrap samples
+Nboot <- 1000
+# sample size
+n <- 300
+
+# model specification
+blip.model <- list(~ O1, ~ O2 + A1)
+treat.model <- list(A1~1, A2~1) 
+tf.model <- list(~ O1, ~ O1 + A1 + O1*A1)
+
+# allocate space
+estm <- vector(mode = "list", length = 1) # m-out-of-n bootstrap
+
+for(i in 1:9) # loop over scenario
+{
+  # reset estimates to NA for new scenario
+  estm[[1]] <- matrix(NA, nrow = Nsimul , ncol = Nboot + 3)
+  
+  for(s in 1:Nsimul) # loop over number of simulations
+  {
+    # treatment A1, A2: P(Aj = 1) = P(Aj = 0) = 0.5
+    A1 <- rbinom(n, size = 1, prob = 0.5)
+    A2 <- rbinom(n, size = 1, prob = 0.5)
+    
+    # covariates O1, O2: coded as -1, 1, where O2 depends on A1, O1 and (delta_1,delta_2)
+    O1 <- 2*rbinom(n, size = 1, prob = 0.5) - 1
+    O2 <- 2*rbinom(n, size = 1, prob = expit(d[sc[i],1]*O1 + d[sc[i],2]*(2*A1 - 1))) - 1
+    
+    # generated outcome Y2 (Y1 set to 0), using parameters (gamma_1,...,gamma_7)
+    Y2 <- g[sc[i],1] + g[sc[i],2]*O1 + g[sc[i],3]*A1 + g[sc[i],4]*O1*A1 + g[sc[i],5]*A2 + g[sc[i],6]*O2*A2 + g[sc[i],7]*A1*A2 + rnorm(n)
+    
+    # generated dataset
+    complete <- cbind(A1, A2, O1, O2, Y2)
+    
+    # fit dWOLS to the generated dataset, using all n=300 observations
+    proba <- list(as.vector(rep(0.5,n)))
+    res.n <- try(DTRreg(outcome = Y2, blip.mod = blip.model, treat.mod = treat.model, tf.mod = tf.model, treat.mod.man = rep(proba,2), method = "dwols", var.estim = "bootstrap", data = as.data.frame(complete)))
+    
+    # save estimates using all observations in the first column
+    estm[[1]][s,1] <- try(extract(res.n))
+    
+    # estimate of nonregularity at stage 2
+    phat <- res.n$nonreg[2]
+    estm[[1]][s,2] <- phat
+    
+    # resampling size
+    m <- n^((1 + alpha*(1-phat))/(1 + alpha))
+    estm[[1]][s,3] <- m
+    
+    # probability treatment with m
+    proba <- list(as.vector(rep(0.5,floor(m))))
+    
+    # bootstrap resampling + estimate
+    for(b in 1:Nboot) # loop over number of bootstrap samples
+    {
+      # resample with replacement: m-out-of-n
+      index <- sample(1:n, floor(m), replace = TRUE)
+      boot <- complete[index,]
+      
+      # fit the model to bootstrap sample: m-out-of-n
+      res <- try(DTRreg(outcome = Y2, blip.mod = blip.model, treat.mod = treat.model, tf.mod = tf.model, treat.mod.man = rep(proba,2), method = "dwols", data = as.data.frame(boot)))
+     
+      # save bootstrap estimates i in the (i+1) column: m-out-of-n
+      estm[[1]][s, b + 3] <- try(extract(res))
+    }
+  }
+  # linux command to save results of the simulations in a CSV file
+  colnames(estm[[1]])[1:3] <- c("psi10","phat","m")
+  name1 <- paste("mn0.1_psi10_scenario", paste(sc[i]),".csv",sep ="")
+  write.csv(estm[[1]], file = name1, row.names = FALSE)
+}
+
+
+
+
+
